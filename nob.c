b441006ed0e2c3b3bb2a5664bcc0eb2ab03df3d1
Alin Odobescu <119128146+ziotonak@users.noreply.github.com>
2026-03-09 04:39:02 +0100

    Move dependencies to vendor/

--- nob.c
+++ nob.c
@@ -2,6 +2,7 @@
 #include "nob.h"
 
 #define BUILD_FOLDER "build/"
+#define VENDOR_FOLDER "vendor/"
 #define SRC_FOLDER "src/"
 
 int main(int argc, char **argv) {
@@ -12,10 +13,21 @@ int main(int argc, char **argv) {
 
     Nob_Cmd cmd = {0};
 
-    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-lm", "-o", BUILD_FOLDER "main",
-            SRC_FOLDER "main.c", SRC_FOLDER "sqlite3.c", SRC_FOLDER "sqlite-vec.c");
+    nob_cmd_append(&cmd, "cc");
+    nob_cmd_append(&cmd, "-Wall", "-Wextra", "-ggdb");
 
-    if (!nob_cmd_run(&cmd))
+    nob_cmd_append(&cmd, "-I", VENDOR_FOLDER);
+    nob_cmd_append(&cmd, "-I", SRC_FOLDER);
+
+    nob_cmd_append(&cmd, "-o", BUILD_FOLDER "main");
+
+    nob_cmd_append(&cmd, SRC_FOLDER "main.c");
+    nob_cmd_append(&cmd, VENDOR_FOLDER "sqlite3.c");
+    nob_cmd_append(&cmd, VENDOR_FOLDER "sqlite-vec.c");
+
+    nob_cmd_append(&cmd, "-lm");
+
+    if (!nob_cmd_run_sync(cmd))
         return 1;
 
     return 0;
